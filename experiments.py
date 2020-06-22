d0e645fcd347dba2e8a921d32aaa1b5a2dad7ada
Guido <guido.v@bit-students.com>
2020-06-22 12:18:50 +0200

    setup for experiments

--- experiments.py
+++ experiments.py
@@ -10,8 +10,10 @@ import tables
 import os
 
 from fa_discrete import *
+from draw import *
 
-def run(eps, n_sites, t_obs, s_):
+
+def run_fixed_eps(run_id, eps, n_sites, t_obs, s_):
     """
     Run an experiment for given parameters and save the results
 
@@ -25,19 +27,19 @@ def run(eps, n_sites, t_obs, s_):
     save_dir = "results/"
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    filepath = "{}/eps={}_nSites={}_tObs={}.h5".format(save_dir, eps, n_sites, t_obs)     
+    filepath = "{}/runID={}_eps={}_nSites={}_tObs={}.h5".format(save_dir, run_id, eps, n_sites, t_obs)     
 
     save_file = tables.open_file(filepath, mode="w")
     atom = tables.Float64Atom()
 
     save_array = save_file.create_earray(save_file.root, 'data', atom,(0, 3))
+    save_array.append(np.array([[eps, n_sites, t_obs]]))
     save_file.close()
 
     for s in s_:
         sfa = SoftenedFA(s=s, eps=eps, gamma=0.25, num_sites=n_sites, num_steps=t_obs, num_burnin_steps=100)
-        trajectory = sfa.gen_trajectory(draw=False)
         sfa_tps = SoftenedFATPS(sfa, sfa.activity)
-        k_avg, k_err = sfa_tps.mc_analysis(100)
+        k_avg, k_err = sfa_tps.mc_analysis(1000, 100, draw=False)
 
         save_obj = np.array([[s, k_avg, k_err]])
 
@@ -50,12 +52,29 @@ def run(eps, n_sites, t_obs, s_):
     return msg
 
 
+def create_s_vals(s_critical, n_s_steps):
+    s_ = [s_critical]
+    for i in reversed(range(int(n_s_steps/2) + 1)):
+        s_.insert(0, s_critical - 0.5*10**-i)
+        s_.append(s_critical + 0.5*10**-i)
+        s_.insert(0, s_critical - 10**-i)
+        s_.append(s_critical + 10**-i)
+
+    return s_
+
+
 if __name__ == "__main__":
 
-    s_ = np.array([-1, 0, 1])
-    msg = run(eps=0, n_sites=10, t_obs=10, s_=s_)
+    s_ = create_s_vals(0, 6)
+    print(s_)
+    msg = run_fixed_eps(run_id=1, eps=0, n_sites=60, t_obs=1000, s_=s_)
 
     filepath = msg.split(":")[1]
     load_file = tables.open_file(filepath, mode="r")
-    print(load_file.root.data[:, :])
+    eps, n_sites, t_obs = load_file.root.data[0, :]
+    data = load_file.root.data[1:, :]
     load_file.close()
+
+    s_vals, k_avgs, k_errs = data.transpose()
+    draw_activity_for_fixed_eps(eps, s_vals, k_avgs, k_errs)
+    plt.show()
--- fa_discrete.py
+++ fa_discrete.py
@@ -1,8 +1,8 @@
 import logging
 
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.optimize as optimize
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 class DiscreteTimeKCM(object):
@@ -499,34 +499,3 @@ def draw_trajectory(trajectory, title, xlabel="Time Step", ylabel="Site", time_s
     ax.set_ylabel(ylabel)
 
     plt.show()
-
-def draw_average_plot(measurements, title, xlabel, ylabel):
-    num_measurements = len(measurements)
-
-    plt.figure(figsize=(20,3))
-    plt.plot(np.arange(num_measurements), measurements)
-    plt.plot(np.arange(num_measurements), np.ones(num_measurements) * np.mean(measurements))
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.ylim(top=1., bottom=0.)
-    plt.show()
-
-def draw_activity_for_fixed_eps(eps, s_vals, activity_means, activity_errs=None):
-    """
-
-    :param (float) eps: The value of epsilon (used for labelling the curve).
-    :param (np.array of shape [num_s_vals, ]) s_vals: The values of the s field
-        for which observables have been measured
-    :param (np.array of shape [num_s_vals, ]) activity_means: The average activity
-        corresponding to the values of s in ``s_vals``
-    :param (Optional np.array of shape [num_s_vals, ]) activity_errors: The error
-        of activity corresponding to the values of s in ``s_vals``. If not provided,
-        defaults to None, and the error bars are not included in the plot.
-    """
-
-    plt.errorbar(s_vals, activity_means, label="$\epsilon={}$".format(eps), yerr=activity_errs)
-    plt.title("Activity as a function of $s$ for fixed $\epsilon$")
-    plt.xlabel("Dynamical Field, $s$")
-    plt.ylabel("Activity, $K$")
-    plt.legend()
